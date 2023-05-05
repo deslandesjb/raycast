c5dc6e4ceb389dc5efd3d9eaa8ef36c9c1a1fd99
Deslandes Jb <47465584+deslandesjb@users.noreply.github.com>
2023-05-05 16:22:09 +0200

    Update path.sh

--- path.sh
+++ path.sh
@@ -13,35 +13,57 @@
 
 # ⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️
 mail="my_email"
-# ⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️
+# ⬆️⬆️⬆️⬆️⬆️⬆️⬆️
 
 mac_user=$(whoami)
-lang=$(defaults read -g AppleLanguages)
-if [[ $lang == *"fr-FR"* ]]; then
-  drive_mac="Drive partagés"
-elif [[ $lang == *"en-EN"* ]]; then
+lang=$(defaults read -g AppleLanguages | tr -d '(),\n" ')
+lang=${lang:0:2}
+
+if [[ $lang == "en" ]]; then
   drive_mac="Shared drives"
+elif [[ $lang == *"fr"* ]]; then
+  drive_mac="Drive partagés"
+elif [[ $lang == *"es"* ]]; then
+  drive_mac="Unidades compartidas"
+elif [[ $lang == *"pt"* ]]; then
+  drive_mac="Drives compartilhados"
+elif [[ $lang == *"de"* ]]; then
+  drive_mac="Freigegebene Laufwerke"
+elif [[ $lang == *"it"* ]]; then
+  drive_mac="Team drive condivisi"
 fi
-drive_windows="Shared drives"
 
 active_app=$(osascript -e 'tell application "System Events" to get name of first application process whose frontmost is true')
 
 if [[ "$active_app" == "Finder" ]]; then
   path=$(osascript -e 'tell application "Finder" to get POSIX path of (target of front window as alias)')
 
-  if [[ $path == *\/*\\* ]]; then
-    echo "The path contains a backslash."
-    exit 1
-  fi
+  special_chars=("\\/" "(" ")" "=" "+" "¨" "^" "°" "*" "‘" "«" "»" "°" "{" "}" "[" "]" "<" ">" "|" "~" "*" "%" "$" "€" "?" ":" ";" "," "#" "\`" "'" "\"")
+  for char in "${special_chars[@]}"; do
+    if [[ $path == *"\\"* ]]; then
+      echo "The path contains a backslash. Please remove it."
+      exit 1
+    elif [[ $path == *"\""* ]]; then
+      echo "The path contains a double quote. Please remove it."
+      exit 1
+    elif [[ $path == *"‘"* ]]; then
+      echo "The path contains a simple quote. Please remove it."
+      exit 1
+    elif [[ $path == *"'"* ]]; then
+      echo "The path contains a simple quote. Please remove it."
+      exit 1
+    elif [[ $path == *"$char"* ]]; then
+      echo "The path contains the character '$char'. Please remove it."
+      exit 1
+    fi
+  done
 
   if [[ $path != *"CloudStorage"* ]]; then
     echo "This is not a Google Drive path."
     exit 1
   fi
-
-  converted_path="G:/$drive_windows/${path#/Users/$mac_user/Library/CloudStorage/GoogleDrive-$mail/$drive_mac/}"
-  converted_path="${converted_path//\//\\}"
-
+  converted_path="G:\\Shared drives\\${path##*/$mac_user/Library/CloudStorage/GoogleDrive-$mail/$drive_mac/}"
+  converted_path=$(echo "$converted_path" | sed 's|/|\\|g')
   echo -n "$converted_path" | pbcopy
   echo "The path was converted for WINDOWS and copied in the clipboard."
 
@@ -49,10 +71,13 @@ else
   clipboard=$(pbpaste)
 
   if [[ $clipboard == G:* ]]; then
-    path_converted=$(echo $clipboard | sed -e 's/\\/\//g' -e "s/G:\/$drive_windows\//\/Users\/$mac_user\/Library\/CloudStorage\/GoogleDrive-$mail\/$drive_mac\//g" )
+    path_converted=$(echo $clipboard | sed -e 's/\\/\//g' -e "s/G:\/Shared drives\//\/Users\/$mac_user\/Library\/CloudStorage\/GoogleDrive-$mail\/$drive_mac\//g" )
 
+    open "$path_converted"
     echo -n "$path_converted" | pbcopy
-    echo "The path was converted for MAC and copied in the clipboard."
+    echo "The file was opened with success."
+
+
   else
     echo "The clipboard does not contain a Google Drive path."
     exit 1
